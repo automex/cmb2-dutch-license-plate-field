bdc4ec8e7d4fbb05ed6651def3e30c7fa2d268ea
Patrick Groot <info@patrickgroot.com>
2020-02-18 23:47:45 +0100

    prefix

--- cmb2-dutch-license-plate.php
+++ cmb2-dutch-license-plate.php
@@ -84,9 +84,9 @@ class AMEX_Dutch_License_Plate {
 
 	public function amex_css_js( $field ) {	
 		$asset_path = apply_filters( 'sa_cmb2_field_faiconselect_asset_path', plugins_url( '', __FILE__ ) );
-		wp_enqueue_style('amex_core_stylesheet', $asset_path . '/assets/css/style.min.css', array(), self::VERSION );
-		wp_enqueue_script('kentekenplaat_js', $asset_path . '/assets/js/kentekenplaat.min.js', array( 'jquery' ), self::VERSION, true );
-		wp_enqueue_script('main_js', $asset_path . '/assets/js/main.js', array( 'jquery' ), self::VERSION, true );
+		wp_enqueue_style('amex_kentekenplaat_css', $asset_path . '/assets/css/style.min.css', array(), self::VERSION );
+		wp_enqueue_script('amex_kentekenplaat_js', $asset_path . '/assets/js/kentekenplaat.min.js', array( 'jquery' ), self::VERSION, true );
+		wp_enqueue_script('amex_main_js', $asset_path . '/assets/js/main.js', array( 'jquery' ), self::VERSION, true );
 	} 
 
 }
